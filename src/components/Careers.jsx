94009148617e467ba9bc5e22865da9c36882ec74
Mohd Uzair <uzairshaikh277002@gmail.com>
2025-05-15 20:43:35 +0530

    Create Careers.jsx

---
+++ src/components/Careers.jsx
@@ -0,0 +1,34 @@
+import React, { useEffect } from 'react';
+import './Careers.css';
+
+export default function Careers() {
+  useEffect(() => {
+    window.scrollTo(0,0);
+  }, []);
+
+  return (
+    <div className="contact-container">
+      <div className="contact-content">
+        <div className="contact-image">
+          <img
+            src="./logo.png"
+            alt="image"
+          />
+        </div>
+        <div className="contact-text">
+          <h2 className="contact-title">
+           Hey There!!!
+          </h2>
+         
+          <p className="contact-paragraph">
+           We will be soon opening positions for you.
+           
+          </p>
+          <p className="contact-paragraph">
+          Please keep us in your touch :)
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
