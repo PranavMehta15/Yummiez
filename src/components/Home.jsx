15e7c7f0b8bc00620b3b74f6036673ccf0789cd8
Rupam0403 <129485743+Rupam0403@users.noreply.github.com>
2025-05-15 10:15:38 +0000

    about us

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,6 +3,7 @@ import "./Home.css";
 import HomeTop from "./HomeTop";
 import HomeBottom from "./HomeBottom";
 import Searchbox from "./Searchbox";
+import About from "./AboutUs";
 
 const cloudinaryBaseUrl = "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/";
 function Home() {
@@ -11,6 +12,7 @@ function Home() {
       <HomeTop />
       <HomeBottom/>
       <Searchbox/>
+      
     </div>  
   );
 }
